71e45fa25b799b8ec51f821803a6da1d59716ce8
Pablo Reyes Betanzo <pabloreyesbetanzo@gmail.com>
2018-11-11 18:38:48 -0300

    Se agrega ESLint

--- src/components/RadioButtonMenu.js
+++ src/components/RadioButtonMenu.js
@@ -1,27 +1,26 @@
-import React from "react";
-import RadioButton from "./RadioButton";
+import React from 'react';
+import RadioButton from './RadioButton';
 
 class RadioButtonMenu extends React.Component {
-    handleButtonChange = index => {
-        this.props.updateSelected(index);
+    handleButtonChange = (index) => {
+      this.props.updateSelected(index);
     };
+
     render() {
-        return (
-            <div className="radio_buttons">
-                {this.props.monedas.map((val, index) => {
-                    return (
-                        <RadioButton
-                            key={index}
-                            index={index}
-                            name="moneda"
-                            label={this.props.monedas[index]}
-                            handleButtonChange={this.handleButtonChange}
-                            selected={this.props.selected}
-                        />
-                    );
-                })}
-            </div>
-        );
+      return (
+        <div className="radio_buttons">
+          {this.props.monedas.map((val, index) => (
+            <RadioButton
+              key={index}
+              index={index}
+              name="moneda"
+              label={this.props.monedas[index]}
+              handleButtonChange={this.handleButtonChange}
+              selected={this.props.selected}
+            />
+          ))}
+        </div>
+      );
     }
 }
 
